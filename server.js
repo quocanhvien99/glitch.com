6f5f099195d1144452fd3c1586b4a240d28edc52
Glitch (chambray-quark-soccer) <none>
2020-04-22 15:03:13 +0000

    🏖🎯 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require('express');
+const app = express();
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get('/todos', (request, response) => {
+  response.send('<html><ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học code tại CodersX</li></ul></html>');
+});
+
+// listen for requests :)
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
