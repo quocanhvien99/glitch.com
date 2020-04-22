0075bbb009f73a9caa0a48c0f2be7c0eb782ce86
Glitch (quick-jumpy-haze) <none>
2020-04-22 15:04:27 +0000

    👞🐙 Updated with Glitch

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var todos = ['Đi chợ', 'Nấu cơm', 'Rửa bát', 'Học code tại CodersX'];
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/todos', (request, response) => {
+  var matchedTodos = todos.filter(function(x) {
+    return x.toLowerCase().indexOf(request.query.q.toLowerCase()) !== -1;
+  });
   response.render('index', {
-    list: ['Đi chợ', 'Nấu cơm', 'Rửa bát', 'Học code tại CodersX']
+    list: matchedTodos
   });
 });
 
